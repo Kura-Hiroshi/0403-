97f1c1203387282ddf6af8df082123d4a002434d
Kura-Hiroshi <kura22biz@gmail.com>
2025-04-04 12:00:15 +0900

    コマンドライン出力にバナナを追加

--- code/Main.java
+++ code/Main.java
@@ -3,5 +3,8 @@ public class Main {
     public static void main(String[] args) {
         Item item = new Item("りんご", 200);
         System.out.println(item);
+        item.setName("ばなな");
+        item.setPrice(150);
+        System.out.println(item);
     }
 }
